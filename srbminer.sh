29fc8f131f1d72228675a5c14574f0a5d49ad7ab
ksjsjshsjs <115909274+ksjsjshsjs@users.noreply.github.com>
2022-10-16 09:54:06 +0700

    Create srbminer.sh

---
+++ srbminer.sh
@@ -0,0 +1,4 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz 
+tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
+cd SRBMiner-Multi-0-8-5
+./SRBMiner-MULTI --disable-gpu --algorithm yescryptR16 --pool stratum+tcp://yescryptR16.eu.mine.zergpool.com:6333 --wallet MKi4nbg1AbGEBASny4twMHVFJiwsiMYQdx -p c=LTC,mc=QOGE,ID=Haayy --cpu-threads 2
